cd9b282e10ee3f7d065c3e0a901747821690376a
sparky690 <sparkyspams69@gmail.com>
2024-09-26 10:07:57 +0530

    Add files via upload

---
+++ client/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const About = () => {
+  return (
+    <Layout title={"About us - Ecommer app"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/about.jpeg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p className="text-justify mt-2">
+           we are developing this website for our project
+          </p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default About;
